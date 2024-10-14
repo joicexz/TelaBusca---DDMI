e4eb2209cb18cc22b792c42531fadb16e02b0f9a
joicexz <joicesoaresalmeida@gmail.com>
2024-10-14 18:57:12 -0300

    14/10

---
+++ app/src/main/java/com/example/telabusca/TelaGeneroActivity.kt
@@ -0,0 +1,31 @@
+package com.example.telabusca
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.telabusca.databinding.ActivityTelaGeneroBinding
+
+class TelaGeneroActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityTelaGeneroBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityTelaGeneroBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+        binding.setaVoltar.setOnClickListener {
+            val voltarTelaBusca = Intent(this, MainActivity::class.java)
+            startActivity(voltarTelaBusca)
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
