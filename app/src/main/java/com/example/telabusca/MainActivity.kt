45c183cec9102d28feda97393c79e496d1d39e67
joicexz <joicesoaresalmeida@gmail.com>
2024-10-20 17:42:37 -0300

    20/10

--- app/src/main/java/com/example/telabusca/MainActivity.kt
+++ app/src/main/java/com/example/telabusca/MainActivity.kt
@@ -18,15 +18,28 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(binding.root)
 
+        val artistas = listOf(
+            Pair(binding.art1, getString(R.string.art1)),
+            Pair(binding.art2, getString(R.string.art2)),
+            Pair(binding.art3, getString(R.string.art3)),
+            Pair(binding.art4, getString(R.string.art4)),
+            Pair(binding.art5, getString(R.string.art5)),
+            Pair(binding.art6, getString(R.string.art6))
+        )
+
+        artistas.forEach { (view, nomeArtista) ->
+            view.setOnClickListener {
+                val navegarTelaArtista = Intent(this, TelaArtistaActivity::class.java)
+                navegarTelaArtista.putExtra("ARTIST_NAME", nomeArtista)
+                startActivity(navegarTelaArtista)
+            }
+        }
+        
         binding.hiphop.setOnClickListener {
             val navegarTelaGenero = Intent(this, TelaGeneroActivity::class.java)
             startActivity(navegarTelaGenero)
         }
 
-        binding.art1.setOnClickListener {
-            val navegarTelaArtista = Intent(this, TelaArtistaActivity::class.java)
-            startActivity(navegarTelaArtista)
-        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/telabusca/TelaArtistaActivity.kt
+++ app/src/main/java/com/example/telabusca/TelaArtistaActivity.kt
@@ -11,16 +11,21 @@ import com.example.telabusca.databinding.ActivityTelaArtistaBinding
 class TelaArtistaActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityTelaArtistaBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTelaArtistaBinding.inflate(layoutInflater)
         enableEdgeToEdge()
         setContentView(binding.root)
 
+        val nomeArtista = intent.getStringExtra("ARTIST_NAME")
+        binding.artista.text = nomeArtista
+
         binding.setaVoltar.setOnClickListener {
             val voltarTelaBusca = Intent(this, MainActivity::class.java)
             startActivity(voltarTelaBusca)
         }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
